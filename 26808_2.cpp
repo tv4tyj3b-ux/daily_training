caaf308e870e03203178301e4048e16525a169d4
BHWR <tv4tyj3b@gmail.com>
2026-08-09 02:01:46 +0800

    Add files via upload

---
+++ 26808_2.cpp
@@ -0,0 +1,15 @@
+//傻逼vs，gbk编码莎莎报错
+//移位操作
+//是我傻逼，文件命名方式有错误
+//掩码和文件操作明天再学吧
+#include <stdio.h>
+int main() {
+    int a = 0x80000000;
+    unsigned int b = 0x80000000;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("a>>1=%d\n",a>>1);
+    printf("b>>1=%d\n",b>>1);
+    printf("我靠搞不完这个我真不睡了");
+    return 0;
+}
